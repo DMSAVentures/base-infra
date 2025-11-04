3688ff3e8771da1f3d3d0d558b8de6b3e74156a3
protoappxyz <me@shubhanshu.dev>
2025-11-04 13:00:15 -0500

    some config fix for vpc

--- vpc.tf
+++ vpc.tf
@@ -49,7 +49,7 @@ resource "aws_subnet" "private_subnet_a" {
   vpc_id                  = aws_vpc.base_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "Private-A"
@@ -61,7 +61,7 @@ resource "aws_subnet" "private_subnet_b" {
   vpc_id                  = aws_vpc.base_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "Private-B"
